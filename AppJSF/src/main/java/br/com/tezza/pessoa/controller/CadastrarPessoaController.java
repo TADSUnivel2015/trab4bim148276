257edbef7a5c71c13f74237b73d59f5e3faed443
Alex Tezza <Alex Tezza@DESKTOP-F4OTEBU>
2016-11-06 14:27:20 -0200

    Parte 07 - Desenvolvido cadastro via upload de arquivo xml. Nesta parte:
    Adicionado a classe CadastrarPessoaController um método que realiza a
    leitura do xml.
    Alterado o arquivo cadastroPessoa.xhtml para exibir uma aba onde é
    possível realizar o carregamento do arquivo xml.

--- AppJSF/src/main/java/br/com/tezza/pessoa/controller/CadastrarPessoaController.java
+++ AppJSF/src/main/java/br/com/tezza/pessoa/controller/CadastrarPessoaController.java
@@ -1,8 +1,20 @@
 package br.com.tezza.pessoa.controller;
 
+import java.io.IOException;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.primefaces.model.UploadedFile;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import br.com.tezza.model.PessoaModel;
 import br.com.tezza.repository.PessoaRepository;
@@ -28,6 +40,9 @@ public class CadastrarPessoaController {
 	@Inject // Realiza a injeção de dependência na variável pessoaRepository.
 	PessoaRepository pessoaRepository;
 
+	// Variável que irá receber o arquivo xml.
+	private UploadedFile file;
+
 	/*
 	 * Getter and Setter
 	 */
@@ -38,6 +53,14 @@ public class CadastrarPessoaController {
 
 	public void setPessoaModel(PessoaModel pessoaModel) {
 		this.pessoaModel = pessoaModel;
+	}
+
+	public UploadedFile getFile() {
+		return file;
+	}
+
+	public void setFile(UploadedFile file) {
+		this.file = file;
 	}
 
 	// Executa a ação de salvar
@@ -60,4 +83,66 @@ public class CadastrarPessoaController {
 
 	}
 
+	// Metódo que carrega o arquivo xml
+	public void UploadRegistros() {
+
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+		try {
+
+			if(this.file.getFileName().equals("")){
+				Uteis.MensagemAtencao("Nenhum arquivo selecionado!");
+				return;
+			}
+
+			DocumentBuilder builder = factory.newDocumentBuilder();
+
+			Document doc = builder.parse(this.file.getInputstream());
+
+			Element element = doc.getDocumentElement();
+
+			NodeList nodes = element.getChildNodes();
+
+			for (int i = 0; i < nodes.getLength(); i++) {
+
+				Node node  = nodes.item(i);
+
+				if(node.getNodeType() == Node.ELEMENT_NODE){
+
+					Element elementPessoa =(Element) node;
+
+					// Pega os valores do arquivo xml.
+					String nome     = elementPessoa.getElementsByTagName("nome").item(0).getChildNodes().item(0).getNodeValue();
+					String sexo     = elementPessoa.getElementsByTagName("sexo").item(0).getChildNodes().item(0).getNodeValue();
+					String email    = elementPessoa.getElementsByTagName("email").item(0).getChildNodes().item(0).getNodeValue();
+					String endereco = elementPessoa.getElementsByTagName("endereco").item(0).getChildNodes().item(0).getNodeValue();
+
+					// Instância um novo objeto do tipo PessoaModel.
+					PessoaModel newPessoaModel = new PessoaModel();
+
+					// Popula o objeto com os valores retirados do xml.
+					newPessoaModel.setUsuarioModel(this.usuarioController.GetUsuarioSession());
+					newPessoaModel.setEmail(email);
+					newPessoaModel.setEndereco(endereco);
+					newPessoaModel.setNome(nome);
+					newPessoaModel.setOrigemCadastro("X");
+					newPessoaModel.setSexo(sexo);
+
+					// Manda salvar o objeto.
+					pessoaRepository.SalvarNovoRegistro(newPessoaModel);
+				}
+			}
+
+			// Notifica o usuário de que a ação foi realizada.
+			Uteis.MensagemInfo("Registros cadastrados com sucesso!");
+
+			// Em caso de erro, lança as exeções abaixo.
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
